1e4d187c65cfd95e5bb1c1c79e59a2da7eea3153
lemus <lemus777@gmail.com>
2022-09-20 23:59:36 +0200

    implementada persistencia en localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de introduccion a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false }
-];
+//const defaultTodos = [
+//  { text: 'Cortar cebolla', completed: true },
+//  { text: 'Tomar el curso de introduccion a React', completed: false },
+//  { text: 'Llorar con la llorona', completed: false }
+//];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  // persistencia
+  const localStorageTodos = localStorage.getItem('TODOS_V1'); // localStorage.getItem recupera lo guardado en el navegador (localStorage).
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([])); // si no hay localStorageTodos generamos uno por defecto que será un array vacío transformado a texto (JSON.stringify) para que localStorage pueda guardarlo
+    parsedTodos = []; // además necesitamos un parsedTodos, que será un array vacío también
+  } else {
+    parsedTodos =JSON.parse(localStorageTodos); // como tenemos un localStorage, que siempre es texto, lo pasamos a array (JSON.parse) y lo usamos como parsedTodos
+  }
+  // fin persistencia
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState(''); // el estado usa un array de searchValue y setSearchValue, es igual a un estado que es un array vacío. Esto se guarda en la constante searchValue, y setSearchValue cambia este estado, por tanto cambia searchValue
 
   const completedTodos = todos.filter(todo => !!todo.completed).length; // filtra nuestros todo y mira si tiene completed como true gracias a !!
@@ -26,18 +38,24 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos) => { // funcion para actualizar estado con persistencia
+    const stringifiedTodos = JSON.stringify(newTodos); // convierte newTodos a texto (newTodos se genera en los métodos para completar y eliminar, mas abajo)
+    localStorage.setItem('TODOS_V1', stringifiedTodos); // guarda en localStorage como item TODOS_V1 el texto generado anteriormente
+    setTodos(newTodos); // actualiza el estado de Todos igualándolo a newTodos
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text); // va a buscarnos el índice del todo cuyo texto coincide con el texto aportado a la funcion
     const newTodos = [...todos]; // vamos a clonar la lista de todos (eso es con el ...)
     newTodos[todoIndex].completed = true; // de esta lista clonada el que tiene el texto coincidente va a ser marcado como completado
-    setTodos(newTodos); //vamos a actualizar el estado de todos para que sea igual a newTodos
+    saveTodos(newTodos); //vamos a actualizar el estado de todos para que sea igual a newTodos y a la vez guarda para persistencia, ver la función más arriba
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text); // va a buscarnos el índice del todo cuyo texto coincide con el texto aportado a la funcion
     const newTodos = [...todos]; // vamos a clonar la lista de todos (eso es con el ...)
     newTodos.splice(todoIndex, 1); // de esta lista clonada el que tiene el texto coincidente va a ser eliminado
-    setTodos(newTodos); //vamos a actualizar el estado de todos para que sea igual a newTodos
+    saveTodos(newTodos); //vamos a actualizar el estado de todos para que sea igual a newTodos
   };
 
   return (
