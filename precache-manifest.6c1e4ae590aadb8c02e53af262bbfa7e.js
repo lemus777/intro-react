451698b8bedc7f281246a198d19de1e30871944c
lemus <lemus777@gmail.com>
2022-09-25 19:47:15 +0200

    Updates

---
+++ precache-manifest.6c1e4ae590aadb8c02e53af262bbfa7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66a8dc05ae2351ee255b",
+    "url": "/intro-react/static/css/main.d1ba0922.chunk.css"
+  },
+  {
+    "revision": "66a8dc05ae2351ee255b",
+    "url": "/intro-react/static/js/main.e9c8060a.chunk.js"
+  },
+  {
+    "revision": "aa8e1f74143b90d1364a",
+    "url": "/intro-react/static/js/runtime~main.37ae2610.js"
+  },
+  {
+    "revision": "0a157e93bb9e4a209cf4",
+    "url": "/intro-react/static/js/2.da256018.chunk.js"
+  },
+  {
+    "revision": "988d653ffdeac0d34a25575d53672edb",
+    "url": "/intro-react/static/media/check.988d653f.svg"
+  },
+  {
+    "revision": "9b8d46c07ebbc7e651133c437f87a194",
+    "url": "/intro-react/static/media/delete.9b8d46c0.svg"
+  },
+  {
+    "revision": "f8ddeb216d7ba741336cae8ebb565991",
+    "url": "/intro-react/index.html"
+  }
+];
